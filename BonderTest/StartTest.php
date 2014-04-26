4384efd561350d3428afa3d46095a9b1b5f52ee7
Dario Ocles <dario.ocles@gmail.com>
2014-04-25 21:48:01 -0300

    testing travis ci service

---
+++ BonderTest/StartTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace BonderTest;
+
+/**
+ * @author burzak
+ * @author hbandura
+ */
+final class StartTest extends \PHPUnit_Framework_TestCase {
+
+  public function testStart() {
+    $this->assertEquals("Start", "Start");
+  }
+
+}
--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-bonder
-======
+# Bonder
 
 The object oriented PHP framework
+
+
+## Install
+
+```bash
+curl -sS https://getcomposer.org/installer | php
+php composer.phar install
+```
