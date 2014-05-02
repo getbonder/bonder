58b5a14297a0e492203e93fa795492144b9d17e9
hbandura <hbandura@gmail.com>
2014-05-02 04:35:15 -0300

    http configuration factory test

---
+++ BonderTest/Http/HttpConfigurationFactoryTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace BonderTest\Http;
+
+final class HttpConfigurationFactoryTest extends \PHPUnit_Framework_TestCase {
+  
+  public function testUriProvider() {
+    $cf = new \Bonder\Http\HttpConfigurationFactory();
+    $this->assertInstanceOf("\Bonder\Http\HttpUriProvider", 
+      $cf->getUriProvider());
+  }
+  
+  public function testRequestFactory() {
+    $cf = new \Bonder\Http\HttpConfigurationFactory();
+    $this->assertInstanceOf("\Bonder\Http\HttpRequestFactory",
+      $cf->getRequestFactory());
+  }
+  
+}
