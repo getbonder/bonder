1b589077c859c133896d368f9d2fbbc412807c40
hbandura <hbandura@gmail.com>
2014-05-03 20:27:00 -0300

    removed starter. added launcher.
    some basic configuration is still missing to ease boiler plate starter code.

---
+++ Bonder/Launcher.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Bonder;
+
+/**
+ * Bonder launcher.
+ * 
+ * @author hbandura
+ */
+final class Launcher {
+  
+  /**
+   * Sets the configuration factory to use.
+   * 
+   * @param \Bonder\ConfigurationFactory $factory the configuration factory.
+   * @return \Bonder\Launcher this.
+   */
+  public function setConfigurationFactory(
+    \Bonder\ConfigurationFactory $factory) {
+    $this->configurationFactory = $factory;
+    return $this;
+  }
+  
+  /**
+   * Returns the configuration factory.
+   * 
+   * @return \Bonder\ConfigurationFactory the configuration factory.
+   */
+  public function getConfigurationFactory() {
+    return $this->configurationFactory;
+  }
+  
+  /**
+   * Sets the filter chain provider to use.
+   * 
+   * @param \Bonder\Filters\FilterChainProvider $filterChainProvider the
+   * filter chain provider.
+   * @return \Bonder\Launcher this.
+   */
+  public function setFilterChainProvider(
+    \Bonder\Filters\FilterChainProvider $filterChainProvider) {
+    $this->filterChainProvider = $filterChainProvider;
+    return $this;
+  }
+  
+  /**
+   * Returns the filter chain provider.
+   * 
+   * @return \Bonder\Filters\FilterChainProvider the filter chain provider.
+   */
+  public function getFilterChainProvider() {
+    return $this->filterChainProvider;
+  }
+  
+  /**
+   * Sets the output stream.
+   * 
+   * @param \Bonder\Stream $output the output stream.
+   * @return \Bonder\Launcher this.
+   */
+  public function setOutputStream(\Bonder\Stream $output) {
+    $this->output = $output;
+    return $this;
+  }
+
+  /**
+   * Returns the output stream.
+   * 
+   * @return \Bonder\Stream the output stream.
+   */
+  public function getOutputStream() {
+    return $this->output;
+  }
+  
+  /**
+   * Launches bonder, and writes the response to the output stream.
+   * 
+   * @return \Bonder\Response the response.
+   */
+  public function launch() {
+    $chainProvider = $this->getFilterChainProvider();
+    $factory = $this->getConfigurationFactory();
+    $uri = $factory->getUriProvider()->getUri();
+    $job = new \Bonder\Process\FactoryJob(
+      $uri,
+      $factory->getRequestFactory(),
+      $chainProvider);
+    $processor = new \Bonder\Process\Processor();
+    $response = $processor->process($job);
+    $response->writeTo($this->getOutputStream());
+    return $response;
+  }
+  
+}
